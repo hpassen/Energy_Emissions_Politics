2d5bb54a648e2b18ce29dc9665ac255da2229e4a
hpassen <passen@uchicago.edu>
2021-01-18 10:34:55 -0600

    started wrangling

---
+++ wrangle.py
@@ -0,0 +1,21 @@
+'''
+Module for wrangling data: cleans and prepares all data sets for analysis 
+'''
+
+import numpy as np
+import pandas as pd
+
+def load_clean_pop(filepath):
+    '''
+    Imports and cleans a census estimates dataframe
+
+    Inputs: 
+        filepath (str): the string for the filepath
+
+    Returns: 
+        pop_df (pandas df): cleaned dataframe of population data
+    '''
+
+    df = pd.read_csv(filepath, header=1)
+
+    return df
